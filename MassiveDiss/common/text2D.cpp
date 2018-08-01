2995a9219070d483ed1fa3177894fc7b089e4ba0
morstius <morstius@gmail.com>
2018-08-01 23:50:27 +0100

    accidentally removed mode switch

--- MassiveDiss/common/text2D.cpp
+++ MassiveDiss/common/text2D.cpp
@@ -18,8 +18,8 @@ unsigned int Text2DUVBufferID;
 unsigned int Text2DShaderID;
 unsigned int Text2DUniformID;
 
-void initText2D(const char * texturePath){
-
+void initText2D(const char * texturePath)
+{
 	// load in texture
 	Text2DTextureID = loadDDS(texturePath);
 
@@ -34,55 +34,55 @@ void initText2D(const char * texturePath){
 	Text2DUniformID = glGetUniformLocation( Text2DShaderID, "texSampler" );
 }
 
-void printText2D(const char * text, int x, int y, int size){
+void populateVector(std::vector<glm::vec2>& vec, const glm::vec2& a, const glm::vec2& b, const glm::vec2& c)
+{
+	vec.push_back(a);
+	vec.push_back(b);
+	vec.push_back(c);
+}
 
-	unsigned int length = (unsigned int)strlen(text);
+void populateVerticesPerLetter(std::vector<glm::vec2>& vertices, const int x, const int y, const unsigned int size, const int letter)
+{
+	glm::vec2 up_left = glm::vec2(x + letter * size, y + size);
+	glm::vec2 up_right = glm::vec2(x + letter * size + size, y + size);
+	glm::vec2 down_right = glm::vec2(x + letter * size + size, y);
+	glm::vec2 down_left = glm::vec2(x + letter * size, y);
 
-	// fill buffers
-	std::vector<glm::vec2> vertices;
-	std::vector<glm::vec2> UVs;
-	for ( unsigned int i=0 ; i<length ; i++ ){
-		
-		glm::vec2 vertex_up_left    = glm::vec2( x+i*size     , y+size );
-		glm::vec2 vertex_up_right   = glm::vec2( x+i*size+size, y+size );
-		glm::vec2 vertex_down_right = glm::vec2( x+i*size+size, y      );
-		glm::vec2 vertex_down_left  = glm::vec2( x+i*size     , y      );
+	// upper triangle
+	populateVector(vertices, up_left, down_left, up_right);
 
-		vertices.push_back(vertex_up_left   );
-		vertices.push_back(vertex_down_left );
-		vertices.push_back(vertex_up_right  );
+	// lower triangle
+	populateVector(vertices, down_right, up_right, down_left);
+}
 
-		vertices.push_back(vertex_down_right);
-		vertices.push_back(vertex_up_right);
-		vertices.push_back(vertex_down_left);
+void populateUVsPerLetter(std::vector<glm::vec2>& uvs, const char character)
+{
+	int numbLetters = 16;
+	float scale = 1.0f / (float)numbLetters;
+	float x = (character % numbLetters) * scale;
+	float y = (character / numbLetters) * scale;
 
-		char character = text[i];
-		float uv_x = (character%16)/16.0f;
-		float uv_y = (character/16)/16.0f;
+	glm::vec2 up_left = glm::vec2(x, y);
+	glm::vec2 up_right = glm::vec2(x + scale, y);
+	glm::vec2 down_right = glm::vec2(x + scale, (y + scale));
+	glm::vec2 down_left = glm::vec2(x, (y + scale));
 
-		glm::vec2 uv_up_left    = glm::vec2( uv_x           , uv_y );
-		glm::vec2 uv_up_right   = glm::vec2( uv_x+1.0f/16.0f, uv_y );
-		glm::vec2 uv_down_right = glm::vec2( uv_x+1.0f/16.0f, (uv_y + 1.0f/16.0f) );
-		glm::vec2 uv_down_left  = glm::vec2( uv_x           , (uv_y + 1.0f/16.0f) );
-		UVs.push_back(uv_up_left   );
-		UVs.push_back(uv_down_left );
-		UVs.push_back(uv_up_right  );
+	// upper triangle
+	populateVector(uvs, up_left, down_left, up_right);
 
-		UVs.push_back(uv_down_right);
-		UVs.push_back(uv_up_right);
-		UVs.push_back(uv_down_left);
-	}
-	glBindBuffer(GL_ARRAY_BUFFER, Text2DVertexBufferID);
-	glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(glm::vec2), &vertices[0], GL_STATIC_DRAW);
-	glBindBuffer(GL_ARRAY_BUFFER, Text2DUVBufferID);
-	glBufferData(GL_ARRAY_BUFFER, UVs.size() * sizeof(glm::vec2), &UVs[0], GL_STATIC_DRAW);
+	// lower triangle
+	populateVector(uvs, down_right, up_right, down_left);
+}
 
-	glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
-
-	// Bind shader
+void draw(GLsizei size)
+{
+	// bind shader
 	glUseProgram(Text2DShaderID);
 
-	// Bind texture
+	// make sure its filling
+	glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+
+	// bind texture
 	glActiveTexture(GL_TEXTURE0);
 	glBindTexture(GL_TEXTURE_2D, Text2DTextureID);
 	glUniform1i(Text2DUniformID, 0);
@@ -90,18 +90,44 @@ void printText2D(const char * text, int x, int y, int size){
 	// 1st attribute buffer : vertices
 	glEnableVertexAttribArray(0);
 	glBindBuffer(GL_ARRAY_BUFFER, Text2DVertexBufferID);
-	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 0, (void*)0 );
+	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 0, (void*)0);
 
 	// 2nd attribute buffer : UVs
 	glEnableVertexAttribArray(1);
 	glBindBuffer(GL_ARRAY_BUFFER, Text2DUVBufferID);
-	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 0, (void*)0 );
+	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 0, (void*)0);
 
 	glEnable(GL_BLEND);
 	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
-	// Draw call
-	glDrawArrays(GL_TRIANGLES, 0, (GLsizei)vertices.size() );
+	// draw call
+	glDrawArrays(GL_TRIANGLES, 0, size);
+}
+
+void printText2D(const char * text, int x, int y, int size)
+{
+	unsigned int length = (unsigned int)strlen(text);
+
+	// fill buffers
+	std::vector<glm::vec2> vertices;
+	std::vector<glm::vec2> UVs;
+
+	for ( unsigned int i=0 ; i<length ; i++ ){
+		
+		populateVerticesPerLetter(vertices, x, y, size, i);
+
+		populateUVsPerLetter(UVs, text[i]);
+	}
+
+	// bind vertex buffer
+	glBindBuffer(GL_ARRAY_BUFFER, Text2DVertexBufferID);
+	glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(glm::vec2), &vertices[0], GL_STATIC_DRAW);
+
+	// bind uv buffer
+	glBindBuffer(GL_ARRAY_BUFFER, Text2DUVBufferID);
+	glBufferData(GL_ARRAY_BUFFER, UVs.size() * sizeof(glm::vec2), &UVs[0], GL_STATIC_DRAW);
+
+	draw((GLsizei)vertices.size());
 
 	glDisable(GL_BLEND);
 
@@ -110,15 +136,15 @@ void printText2D(const char * text, int x, int y, int size){
 
 }
 
-void cleanupText2D(){
-
-	// Delete buffers
+void cleanupText2D()
+{
+	// delete buffers
 	glDeleteBuffers(1, &Text2DVertexBufferID);
 	glDeleteBuffers(1, &Text2DUVBufferID);
 
-	// Delete texture
+	// delete texture
 	glDeleteTextures(1, &Text2DTextureID);
 
-	// Delete shader
+	// delete shader
 	glDeleteProgram(Text2DShaderID);
 }
